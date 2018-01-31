376b72558da0ad2eb3f73181f90006d5d5a83d3d
Julian Mahecha <jmahecha@belatrixsf.com>
2018-01-31 16:32:27 -0500

    section 10

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,56 +1,28 @@
-import React, {PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import * as courseAction from '../../actions/courseAction';
+import CourseList from './CourseList';
+import CourseListRow from './CourseListRow';
 
 class CoursesPage extends React.Component {
-  constructor (props, context) {
+  constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: { title: ""}
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
   }
 
-  onTitleChange (event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course:course});
-  }
-  onClickSave () {
-    // alert(`Saving ${this.state.course.title}`)
-    this.props.actions.createCourse(this.state.course);
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
-  }
-
-  render () {
+  render() {
+    const {courses} = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange= {this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
+        <CourseList courses={courses}/>
       </div>
     );
   }
 }
 
 CoursesPage.propTypes = {
-  courses:PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
 };
 
