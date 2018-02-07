72fcce78ff211774ea59e243196ba83e4c9d9854
Julian Mahecha <jmahecha@belatrixsf.com>
2018-02-07 11:15:22 -0500

    section 14

---
+++ src/actions/courseAction.test.js
@@ -0,0 +1,19 @@
+import expect from 'expect';
+import * as courseActions from './courseAction';
+import * as types from './actionTypes';
+
+describe('Course Actions', ()=> {
+  describe('createCourseSucesss', () => {
+    it('should create a CREATE_COURSE_SUCCESS action', () => {
+      const course = {id: 'clean-code', title: 'Clean Code'};
+      const expectedAction = {
+        type: types.CREATE_COURSE_SUCCESS,
+        course: course
+      };
+
+      const action = courseActions.createCourseSuccess(course);
+
+      expect(action).toEqual(expectedAction);
+    })
+  });
+});
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -4,8 +4,9 @@ import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseAction';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
+import { authorsFormattedForDropdown } from '../../selectors/selectors';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
@@ -21,7 +22,7 @@ class ManageCoursePage extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if (this.props.course.id != nextProps.course.id) {
-      this.setState({course: Object.assign({}, nextProps.course)});
+      this.setState({ course: Object.assign({}, nextProps.course) });
     }
   }
 
@@ -32,20 +33,36 @@ class ManageCoursePage extends React.Component {
     return this.setState({ course: course });
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
   saveCourse(event) {
     event.preventDefault();
-    this.setState({saving: true});
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
+    this.setState({ saving: true });
     this.props.actions.saveCourse(this.state.course)
-    .then(() => this.redirect())
-    .catch(error => {
-      toastr.error(error);
-      this.setState({saving: false});
-    });
+      .then(() => this.redirect())
+      .catch(error => {
+        toastr.error(error);
+        this.setState({ saving: false });
+      });
 
   }
 
-  redirect () {
-    this.setState({saving: false});
+  redirect() {
+    this.setState({ saving: false });
     toastr.success('Course Saved');
     this.context.router.push('/courses');
   }
@@ -95,16 +112,11 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
+
 
   return {
     course: course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
